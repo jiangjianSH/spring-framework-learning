7961ed9a992b14834c9d64d43d4dfe4b669aa93e
jiangjian <jiangjian199204@gmail.com>
2018-09-27 18:04:18 +0800

    data access jdbc [step1] config JdbcTemplate

---
+++ src/main/java/com/together/learning/spring/dataaccess/jdbc/step1_config_jdbctemplate/ConfigJdbcTemplateSample.java
@@ -0,0 +1,21 @@
+package com.together.learning.spring.dataaccess.jdbc.step1_config_jdbctemplate;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @author jiangjian
+ */
+public class ConfigJdbcTemplateSample {
+    public static void main(String[] args) {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(Config.class);
+        JdbcTemplate jdbcTemplate = ac.getBean(JdbcTemplate.class);
+
+        jdbcTemplate.execute("drop table user");
+        jdbcTemplate.execute("create table user(id int auto_increment primary key, name varchar(40))");
+
+        jdbcTemplate.execute("drop table user");
+
+    }
+}
